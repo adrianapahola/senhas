17923db26e087d269e209ca8bc33a8d9842a1c63
adrianapahola <129787720+adrianapahola@users.noreply.github.com>
2024-07-31 16:16:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const numeroSenha = documentSelector('.parametro-senha__texto');
+const numeroSenha = document.getElementById('parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 const letrasMaisculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -61,4 +61,17 @@ function geraSenha() {
 
 }
 
-function classificaSenha()
+function classificaSenha(tamanhoAlfabeto){   
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia >35 && entropia < 57) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('forte');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 'Um computador pode levarate' + Math.floor(2**entropia/(100e6*60*60*24)) + ' dias para descobrir essa senha';
+}
